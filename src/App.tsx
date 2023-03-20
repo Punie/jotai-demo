790acbf15e4cb21b3c4a574e042336ac47f743c4
Hugo Saracino <hugo.saracino@gmail.com>
2023-03-20 13:51:08 +0100

    🎉 Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="max-w-7xl min-h-full mx-auto py-6 flex flex-col gap-12">
+      <h1 className="text-5xl font-bold text-center">Jotai demo</h1>
+    </div>
+  );
+}
